1757975994ba0034ca0a49d104bb6a44eb549ad5
Christian Zirkelbach <czi@informatik.uni-kiel.de>
2019-03-19 15:51:01 +0100

    adapting more dynamic extension mechanism in frontend

--- addon/instance-initializers/add-extension.js
+++ addon/instance-initializers/add-extension.js
@@ -2,9 +2,9 @@ import Router from "explorviz-frontend/router";
 
 export function initialize(appInstance) {
 
-  // configuration plugin description object  
+  // configuration extension description object  
   // contains the name of it's name and it's title
-  const pluginDescription = {
+  const extensionDescription = {
     id: 'colorpicker',
     title: 'Color Picker',
     link: 'configuration.colorpicker',
@@ -12,29 +12,16 @@ export function initialize(appInstance) {
     paneName: 'colorPickerPane'
   };
 
+  // adding the extension as a tab in the configuration ui
   const service = appInstance.lookup('service:configuration');
-  service.get('pluginSettings').addObject(pluginDescription);
+  service.get('configurationExtensions').addObject(extensionDescription);
 
-  // add subroute to route 'confguration'
-  Router.map(function () {
-
-    // TODO 
-    // working - if the whole configuration is used from frontend and colorpicker is added
-    this.route('configuration', function() {
-      this.route('usermanagement', function() {
-        this.route('users');
-        this.route('edit', { path: '/edit/:user_id' });
-        this.route('new');
-      });
-      this.route('settings');
-      this.route(pluginDescription.nestedRoute);
-    });
-
-  });
-
+  Router.configurationRouteExtensions.push('colorpicker');
+  console.log('initializer add-extension.js');
 }
 
 export default {
   name: 'explorviz-frontend-extension-colorpicker',
+  after: 'router',
   initialize
 };
